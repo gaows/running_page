be6eeb02852a75daae78111163307db15374d84e
weishan <timothy.gao@hotmail.com>
2022-12-14 22:38:12 +0800

    Update const.js

--- src/utils/const.js
+++ src/utils/const.js
@@ -11,16 +11,17 @@ const MUNICIPALITY_CITIES_ARR = [
 ];
 
 // styling: set to `true` if you want dash-line route
-const USE_DASH_LINE = true;
+const USE_DASH_LINE = false;
 // styling: route line opacity: [0, 1]
-const LINE_OPACITY = 0.4;
+const LINE_OPACITY = 0.5;
 
 // IF you outside China please make sure IS_CHINESE = false
 const IS_CHINESE = true;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength, year) => {
   const yearStr = year === 'Total' ? '所有' : ` ${year} `;
-  return `我用 App 记录自己跑步 ${yearLength} 年了，下面列表展示的是${yearStr}的数据`;
+  return `我用 App 记录自己跑步 ${yearLength} 年了，下面列表展示的是${yearStr}的数据。
+  NO Human is limited.`;
 }
 const ENGLISH_INFO_MESSAGE = (yearLength, year) =>
   `Running Journey with ${yearLength} Years, the table shows year ${year} data`;
@@ -62,7 +63,7 @@ export {
   LINE_OPACITY,
 };
 
-const nike = 'rgb(224,237,94)'; // if you want change the main color change here src/styles/variables.scss
+const nike = 'rgb(255,51,153)'; // if you want change the main color change here src/styles/variables.scss
 
 // If your map has an offset please change this line
 // issues #92 and #198
