0f548be88772d978bcd5808c57e7fbd71737fe75
Shan <3062898+gaows@users.noreply.github.com>
2025-12-23 22:24:34 +0800

    Update config.py

--- run_page/config.py
+++ run_page/config.py
@@ -1,28 +1,12 @@
 import os
-from collections import namedtuple
 
-# getting content root directory
-current = os.path.dirname(os.path.realpath(__file__))
-parent = os.path.dirname(current)
+# Configuration file paths
+JSON_FILE = os.path.join(os.getcwd(), 'src/static/activities.json')
+SQL_FILE = os.path.join(os.getcwd(), 'run_page/data.db')
 
-OUTPUT_DIR = os.path.join(parent, "activities")
-GPX_FOLDER = os.path.join(parent, "GPX_OUT")
-TCX_FOLDER = os.path.join(parent, "TCX_OUT")
-FIT_FOLDER = os.path.join(parent, "FIT_OUT")
-PNG_FOLDER = os.path.join(parent, "PNG_OUT")
-ENDOMONDO_FILE_DIR = os.path.join(parent, "Workouts")
-FOLDER_DICT = {
-    "gpx": GPX_FOLDER,
-    "tcx": TCX_FOLDER,
-    "fit": FIT_FOLDER,
-}
-SQL_FILE = os.path.join(parent, "run_page", "data.db")
-JSON_FILE = os.path.join(parent, "src", "static", "activities.json")
-SYNCED_FILE = os.path.join(parent, "imported.json")
-
-
-BASE_TIMEZONE = "Asia/Shanghai"
-UTC_TIMEZONE = "UTC"
-
-start_point = namedtuple("start_point", "lat lon")
-run_map = namedtuple("polyline", "summary_polyline")
+# API Credentials
+STRAVA_CLIENT_ID = "your_strava_client_id"
+STRAVA_CLIENT_SECRET = "your_strava_client_secret"
+STRAVA_REFRESH_TOKEN = "your_strava_refresh_token"
+GARMIN_SECRET_STRING = "your_garmin_secret_string"
+GARMIN_CN_SECRET_STRING = "your_garmin_cn_secret_string"
