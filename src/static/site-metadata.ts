e098893f6b2bbeae56da4357e3ec58b8a2b19514
Shan <3062898+gaows@users.noreply.github.com>
2025-12-23 22:05:24 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,9 +15,9 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteTitle: 'The Flash',
+  siteUrl: 'https://gaows.github.io/running_page/',
+  logo: 'https://cdn.gtn9.com/23-05-2025/754BD02A7C0047E088B28875A44216.png',
   description: 'Personal site and blog',
   navLinks: [
     {
@@ -25,12 +25,8 @@ const data: ISiteMetadataResult = {
       url: `${getBasePath()}/summary`,
     },
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://about.me/gaows',
     },
   ],
 };
