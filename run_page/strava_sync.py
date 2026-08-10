29ae8ec88028752f9871aa08a83cd70f368986ea
Shan <3062898+gaows@users.noreply.github.com>
2026-08-11 01:04:46 +0800

    sync: update run_page/strava_sync.py from upstream for consistency

--- run_page/strava_sync.py
+++ run_page/strava_sync.py
@@ -5,11 +5,20 @@ from config import JSON_FILE, SQL_FILE
 from generator import Generator
 
 
-def run_strava_sync(client_id, client_secret, refresh_token, sync_types: list = ["running"], only_run=False):
+# for only run type, we use the same logic as garmin_sync
+def run_strava_sync(
+    client_id,
+    client_secret,
+    refresh_token,
+    sync_types: list = [],
+    only_run=False,
+):
     generator = Generator(SQL_FILE)
     generator.set_strava_config(client_id, client_secret, refresh_token)
+    # judge sync types is only running or not
     if not only_run and len(sync_types) == 1 and sync_types[0] == "running":
         only_run = True
+    # if you want to refresh data change False to True
     generator.only_run = only_run
     generator.sync(False)
 
